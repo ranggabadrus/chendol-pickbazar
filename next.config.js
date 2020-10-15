639cc33cfa529584cafcdddd4b140afed4d7c601
Yu Xiang <sohyuxiang@chendol.com>
2020-10-15 20:32:33 +0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const withPlugins = require("next-compose-plugins");
+const withOptimizedImages = require("next-optimized-images");
+
+// next.js configuration
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/furniture",
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = withPlugins([withOptimizedImages], nextConfig);
